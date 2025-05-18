d2f7657ad0a928d368af2e90cc4893416619f901
FIONA\dunte <fionaseah98@gmail.com>
2025-05-18 13:05:08 +0800

    feat: add api on index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const datasetId = "d_02d07531b84cd0cf2cf901fc1bf5d395";
+const url =
+  "https://data.gov.sg/api/action/datastore_search?resource_id=" + datasetId;
+
 console.log("Fetching data...");
+
+// Fetch data from the API
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data.result.records);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
