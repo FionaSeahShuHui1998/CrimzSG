83b85cfb8e563860bf707e90a9aeb441c2e42709
FIONA\dunte <fionaseah98@gmail.com>
2025-05-22 14:18:46 +0800

    feat: update line graph with actual data

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// Declaration of variables
+const xAxis = [];
+const labels = [];
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "brown",
+  "gray",
+  "cyan",
+  "pink",
+  "black",
+];
+const dataArray = [];
+const datasets = [];
+
+// API URL and dataset ID
 const datasetId = "d_02d07531b84cd0cf2cf901fc1bf5d395";
 const url =
   "https://data.gov.sg/api/action/datastore_search?resource_id=" + datasetId;
@@ -13,37 +32,49 @@ fetch(url)
     return response.json();
   })
   .then((data) => {
+    // Successfully fetched data
     console.log(data.result.records);
+    // Extracting data for Line Graph
+    // Getting types of crimes
+    for (let i = 1; i < data.result.records.length; i++) {
+      labels.push(data.result.records[i].DataSeries);
+    }
+
+    for (let i = 1; i < data.result.records.length - 2; i++) {
+      dataArray.push(data.result.records[i]);
+    }
+    dataArray.forEach((obj) => {
+      delete obj._id;
+      delete obj.DataSeries;
+    });
+
+    for (let i = 0; i < dataArray.length; i++) {
+      datasets.push({
+        label: labels[i],
+        data: dataArray[i],
+        borderColor: colors[i % colors.length],
+        fill: false,
+      });
+    }
+    const myChart = new Chart(document.getElementById("myChart"), {
+      type: "line",
+      data: {
+        datasets: datasets,
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top",
+          },
+          title: {
+            display: true,
+            text: "Most Common Crime in Singapore",
+          },
+        },
+      },
+    });
   })
   .catch((error) => {
     console.error("Error fetching data:", error);
   });
-
-const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May"], // X-axis labels
-    datasets: [
-      {
-        label: "Line A",
-        data: [10, 20, 15, 30, 25],
-        borderColor: "blue",
-        fill: false,
-      },
-      {
-        label: "Line B",
-        data: [5, 15, 10, 20, 18],
-        borderColor: "red",
-        fill: false,
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    plugins: {
-      legend: { position: "top" },
-      title: { display: true, text: "Simple Multi-Line Graph" },
-    },
-  },
-});
